8465c465b7e9e9381455b3b66807f21184d3c774
DaisukeAoki <dice.k.aoki@gmail.com>
2016-08-21 14:29:26 +0900

    add csv 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(email: 'aoki_in_australia@yahoo.co.jp' ,password: 'btcs0822', nickname: 'dice-k')
 
 require "csv"
+# words_csv = CSV.readlines("db/words.csv")
+# words_csv.shift
+# words_csv.each do |row|
+# Word.create(words: row[1], meaning: row[2], wrong1: row[3], wrong2: row[4]) # end
 
-words_csv = CSV.readlines("db/words.csv", encoding:'Shift_JIS:UTF-8')
-words_csv.shift
-words_csv.each do |row|
-  Word.create(words: row[1], meaning: row[2], wrong1: row[3], wrong2: row[4])
-end
+CSV.foreach('db/words.csv', encoding: 'Shift_JIS:UTF-8') do |row|
+Model.create(words: row[1], meaning: row[2], wrong1: row[3], wrong2: row[4]) end
