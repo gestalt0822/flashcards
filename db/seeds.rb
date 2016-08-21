da08b3ba2cb564b6a9d913ca50a668009c804c5d
DaisukeAoki <dice.k.aoki@gmail.com>
2016-08-21 13:19:43 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(email: 'aoki_in_australia@yahoo.co.jp' ,password: 'btcs0822', nickname: 'dice-k')
+
+require "csv"
+words_csv = CSV.readlines("db/words.csv")
+words_csv.shift
+words_csv.each do |row|
+  Word.create(words: row[1], meaning: row[2], wrong1: row[3], wrong2: row[4])
+end
