f4138d67ca8e233fcd9eb3e7207505181eeb36e0
Daisuke <dice.k.aoki@gmail.com>
2016-08-24 04:37:55 +0100

    resrouces users 2

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :user , only: :show
+  resources :users , only: :show
   get 'words/ranking' => 'words#ranking'
   get 'quiz/quiz' => 'quiz#quiz'
   get 'quiz/question' => 'quiz#question'
